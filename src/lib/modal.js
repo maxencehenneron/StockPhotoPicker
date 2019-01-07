dd38273c0047e3ad5e50808ede19a13f15aaacb7
Maxence Henneron <maxence.henneron@icloud.com>
2019-01-07 20:53:46 +0200

    First version

---
+++ src/lib/modal.js
@@ -0,0 +1,150 @@
+export default class Modal {
+  constructor(options) {
+    this.options = options;
+    this.lastUpdate = new Date();
+    this.renderModal();
+  }
+
+  // MARKER: - Rendering
+
+  /**
+   * Creates the base modal
+   */
+  renderModal() {
+    const { onBackgroundTap } = this.options;
+
+    this.rootElem = document.createElement('div');
+    this.rootElem.classList.add('photo-picker-modal');
+    this.rootElem.onclick = (elem) => {
+      if (elem.target !== this.rootElem) {
+        return;
+      }
+      onBackgroundTap();
+    };
+
+    this.contentElem = document.createElement('div');
+    this.contentElem.classList.add('photo-picker-modal-content');
+
+    this.renderSearchBar(this.contentElem);
+    this.renderGrid();
+
+    this.rootElem.appendChild(this.contentElem);
+  }
+
+  /**
+   * Creates the grid and appends the columns to it
+   */
+  renderGrid() {
+    this.columns = [];
+
+    const gridElem = document.createElement('div');
+    gridElem.classList.add('photo-picker-grid');
+    gridElem.onscroll = (el) => { this.onScroll(el.target); };
+
+    let i;
+    for (i = 0; i < 3; i += 1) {
+      const column = document.createElement('div');
+      column.classList.add('photo-picker-column');
+      gridElem.appendChild(column);
+      this.columns.push(column);
+    }
+
+    this.contentElem.appendChild(gridElem);
+  }
+
+  /**
+   * Renders the top search bar
+   */
+  renderSearchBar(contentElem) {
+    const { placeholder, onSearch, image } = this.options;
+
+    const searchWrapper = document.createElement('div');
+    searchWrapper.classList.add('photo-picker-search-container');
+
+    const searchInput = document.createElement('input');
+    searchInput.type = 'text';
+    searchInput.classList.add('search-bar');
+    searchInput.placeholder = placeholder || 'Search something awesome';
+    searchWrapper.appendChild(searchInput);
+
+    const providerImage = document.createElement('img');
+    providerImage.src = image;
+    providerImage.classList.add('photo-picker-provider-icon');
+    searchWrapper.appendChild(providerImage);
+
+    searchInput.oninput = () => {
+      // Adds a delay to prevent abusive API requests
+      clearTimeout(this.delayTimer);
+      this.delayTimer = setTimeout(() => {
+        this.resetGrid();
+        onSearch(searchInput.value);
+      }, 750);
+    };
+
+    contentElem.appendChild(searchWrapper);
+  }
+
+  // MARKER: - Public functions
+
+  /**
+   * Appends the photos in the grid
+   * @param {photos} An object that contains all the photos to add in the grid
+   */
+  addPhotos(photos) {
+    photos.forEach((value) => {
+      const photoContainer = document.createElement('div');
+      photoContainer.classList.add('photo-picker-photo');
+
+      const photo = document.createElement('img');
+      photo.src = value.small;
+      photoContainer.appendChild(photo);
+
+      photo.onload = () => {
+        this.getSmallestColumn().appendChild(photoContainer);
+      };
+    });
+  }
+
+  /**
+   * Attaches the modal to the DOM, at the end of the body.
+   */
+  attach() {
+    document.body.appendChild(this.rootElem);
+  }
+
+  /**
+   * Detaches the modal to the DOM
+   */
+  detach() {
+    this.rootElem.remove();
+  }
+
+  // MARKER - Event handlers
+  onReachBottom() {
+    const { onReachBottom } = this.options;
+    if ((new Date().getTime() - this.lastUpdate.getTime()) > 1000) {
+      this.lastUpdate = new Date();
+      onReachBottom();
+    }
+  }
+
+  onScroll(el) {
+    // visible height + pixel scrolled = total height
+    if (el.offsetHeight + el.scrollTop >= 0.80 * el.scrollHeight) {
+      this.onReachBottom();
+    }
+  }
+
+  // MARKER - Helpers
+  resetGrid() {
+    this.columns.forEach((col) => {
+      while (col.firstChild) {
+        col.removeChild(col.firstChild);
+      }
+    });
+  }
+
+  getSmallestColumn() {
+    return this.columns.sort((a, b) => (a.scrollHeight - b.scrollHeight))[0];
+  }
+}
--- src/lib/stock-photo-picker.js
+++ src/lib/stock-photo-picker.js
@@ -1,8 +1,63 @@
+import Modal from './modal';
+import Unsplash from './providers/unsplash';
+import UnsplashImage from '../img/unsplash.svg';
+import Giphy from './providers/giphy';
+import GiphyImage from '../img/giphy.png';
+
+const providers = {
+  unsplash: {
+    api: Unsplash,
+    image: UnsplashImage,
+  },
+  giphy: {
+    api: Giphy,
+    image: GiphyImage,
+  },
+};
+
 export default class StockPhotoPicker {
-  static helloWorld(sender) {
-    const el = document.createElement('div');
-    el.classList.add('hello-world');
-    el.innerHTML = 'From the widget!';
-    sender.appendChild(el);
+  constructor(config) {
+    this.config = config;
+  }
+
+  open(options) {
+    this.provider = options.provider;
+
+    if (this.opened) {
+      return;
+    }
+
+    this.modal = new Modal({
+      onSearch: (value) => {
+        this.currentPage = 1;
+        this.searchText(value);
+      },
+      onReachBottom: () => {
+        this.currentPage += 1;
+        this.searchText(this.currentSearchText);
+      },
+      onBackgroundTap: () => {
+        this.close();
+      },
+      image: providers[this.provider].image,
+    });
+
+    this.modal.attach();
+    this.opened = true;
+  }
+
+  close() {
+    this.modal.detach();
+    this.opened = false;
+  }
+
+  searchText(value) {
+    this.currentSearchText = value;
+
+    providers[this.provider].api(this.config[this.provider].token, value, this.currentPage).then(
+      (photos) => {
+        this.modal.addPhotos(photos);
+      },
+    );
   }
 }
